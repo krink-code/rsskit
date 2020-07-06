81238221a1dfb5fa1146d0f879a72cb25125a7a2
Karl Rink <karl@rink.us>
2020-07-06 12:21:13 -0700

    post_msteams.py

---
+++ post_msteams.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import select
+
+if not select.select([sys.stdin,],[],[],0.0)[0]:
+    print(json.dumps({"Error":"no input data"}))
+    sys.exit(1)
+
+try:
+    values = json.load(sys.stdin)
+except ValueError as e:
+    print(json.dumps({"Error":"invalid json data"}))
+    sys.exit(1)
+
+import urllib.parse
+import urllib.request
+
+if sys.argv[1:]:
+    url = sys.argv[1]
+else:
+    print(json.dumps({"Error":"no url"}))
+    sys.exit(1)
+
+params = json.dumps(values).encode('utf8')
+
+req = urllib.request.Request(url, data=params, headers={'content-type': 'application/json'})
+with urllib.request.urlopen(req) as response:
+    the_response = response.read()
+
+print(str(the_response))
+
+
